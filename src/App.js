d85141b07c8db920fcf1ceb6867873400ac82f56
ChazzitUP <hamzarezgui.education@gmail.com>
2023-07-26 15:41:53 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Product from "./pages/Product/Product";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 function App() {
-  return <div>Hello world!</div>;
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/products/:id",
+          element: <Products />,
+        },
+        {
+          path: "/product/:id",
+          element: <Product />,
+        },
+      ],
+    },
+  ]);
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
