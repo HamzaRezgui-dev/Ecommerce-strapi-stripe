c07776b5a98116acc27519a548e3d1a32c836d9d
ChazzitUP <hamzarezgui.education@gmail.com>
2023-07-27 17:45:17 +0200

    categories grid layout

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Categories.scss";
+const Categories = () => {
+  return (
+    <div className="categories">
+      <div className="col">
+        <div className="row">
+          <img
+            src="https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          />
+          <button>
+            <Link className="link" to={"/products/1"}>
+              Sales
+            </Link>
+          </button>
+        </div>
+        <div className="row">
+          <img
+            src="https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          />
+          <button>
+            <Link className="link" to={"/products/1"}>
+              Women
+            </Link>
+          </button>
+        </div>
+      </div>
+      <div className="col">
+        <div className="row">
+          {" "}
+          <img
+            src="https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          />
+          <button>
+            <Link className="link" to={"/products/1"}>
+              Shoes
+            </Link>
+          </button>
+        </div>
+      </div>
+      <div className="col col-l">
+        <div className="row">
+          <div className="col">
+            <div className="row">
+              <img
+                src="https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <button>
+                <Link className="link" to={"/products/1"}>
+                  Men
+                </Link>
+              </button>
+            </div>
+          </div>
+          <div className="col">
+            <div className="row">
+              <img
+                src="https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <button>
+                <Link className="link" to={"/products/1"}>
+                  Children
+                </Link>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          {" "}
+          <img
+            src="https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          />
+          <button>
+            <Link className="link" to={"/products/1"}>
+              Weak
+            </Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
